dd3332684a6cd3a7470357b20654ad2bf9a6c6ec
Jon Principe <jonathan.principe@gmail.com>
2020-11-29 22:12:51 -0500

    mix phx.gen.live Accounts User users name:string age:integer

--- lib/surface_demo_web.ex
+++ lib/surface_demo_web.ex
@@ -83,6 +83,7 @@ defmodule SurfaceDemoWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import SurfaceDemoWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
